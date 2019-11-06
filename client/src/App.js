2d54dbcedbc818682b95a7d902bc129223daba51
j m <jm@js-macbook-pro.home>
2019-11-06 12:45:31 +0100

    clear button works

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ContactState from './context/contact/ContactState'
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import './App.css';
 
+
 const App =() => {
   return (
-    <>
-      <Navbar/>
-      <div className="container">
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About}/>
-        </Switch>
-      </div>
-    </>
+    <ContactState>
+      <Router>
+        <>
+          <Navbar/>
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About}/>
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </ContactState>
   );
 }
 
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,88 @@
+import React, { useReducer } from 'react'
+import uuid from 'uuid'
+import ContactContext from './contactContext'
+import contactReducer from './contactReducer'
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from '../types'
+
+//Create initial state
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill Johnson',
+        email: 'jill@gmail.com',
+        phone: '222-222-2222',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'Mary Smith',
+        email: 'smith@gmail.com',
+        phone: '223-223-2223',
+        type: 'personal'
+      },
+      {
+        id: 3,
+        name: 'Karl White',
+        email: 'karl@gmail.com',
+        phone: '122-122-1222',
+        type: 'professional'
+      }
+    ],
+    current: null
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add contact
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
+
+  // Delete contact
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id })
+  };
+  // Set current Contact
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact })
+  };
+
+  // Clear current contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT}) // no payload, setting up to null
+  }
+
+  // Update contact
+
+  // Filter contacts
+
+  // Clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent
+    }}>
+      {props.children}
+    </ContactContext.Provider>
+  )
+
+}
+
+export default ContactState

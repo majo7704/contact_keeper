0da34660bffcc66ab3af4098a6ec7b392852fb01
j m <jm@js-macbook-pro.home>
2019-11-13 14:32:36 +0100

    working on registration and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ContactState from './context/contact/ContactState'
+import AuthState from './context/auth/AuthState'
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+import Register from './components/auth/Register'
 import './App.css';
 
 
 const App =() => {
   return (
+  <AuthState>
     <ContactState>
       <Router>
         <>
@@ -16,12 +19,14 @@ const App =() => {
           <div className="container">
             <Switch>
               <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About}/>
+                <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register}/>
             </Switch>
           </div>
         </>
       </Router>
-    </ContactState>
+      </ContactState>
+    </AuthState>
   );
 }
 
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,7 +38,8 @@ const ContactState = props => {
         type: 'professional'
       }
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -68,21 +69,30 @@ const ContactState = props => {
     dispatch({ type: UPDATE_CONTACT, payload: contact })
   };
 
-
   // Filter contacts
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text })
+  };
 
   // Clear filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER }) // no payload, setting up to null
+  }
+
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         updateContact,
         deleteContact,
         setCurrent,
-        clearCurrent
+        clearCurrent,
+        filterContacts,
+        clearFilter
     }}>
       {props.children}
     </ContactContext.Provider>
